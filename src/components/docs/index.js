b881d83421f4ea793b931a997f71462b81405087
MiguelWanderer <subterra.1999@gmail.com>
2026-07-14 21:52:27 -0400

    Imagenes...

---
+++ src/components/docs/index.js
@@ -0,0 +1,5 @@
+﻿export { default as SoftwareLibreLicencias } from './SoftwareLibreLicencias';
+export { default as InstalacionBasica } from './InstalacionBasica';
+export { default as PermisosComandos } from './PermisosComandos';
+export { default as GestoresPaquetes } from './GestoresPaquetes';
+export { default as NginxDespliegue } from './NginxDespliegue';
